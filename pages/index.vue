00219c8b5f7cdbec97894d8b8d8f163a769c3ba2
john <aarontu803@gmail.com>
2024-03-24 21:56:49 +0800

    init

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="container">
-        <h1 class="text-lime-500 p-2">{{ title }}</h1>
+    <div class="container ">
+        <iframe class="w-3/4 mx-auto aspect-[9/16]" src="https://www.youtube.com/embed/bbDjmuFt7dE?si=lStV-3FGy9eHo7cu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     </div>
 </template>
 
